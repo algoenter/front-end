66ddf1c5083017c7a34cf88840a8cbcf3d022d33
l <l@aaa.ipv6.infinitum.net.mx>
2024-05-10 10:21:25 -0600

    Subiendo version 2

---
+++ precache-manifest.789b750b763098bdb97f587de988e432.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f57ad3394c664f5bb78ae6e3767027",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e20521c326b88172a5fa",
+    "url": "/static/css/2.fa43bc9e.chunk.css"
+  },
+  {
+    "revision": "0b9eae6ed64b2de78d75",
+    "url": "/static/css/main.eae6dd0d.chunk.css"
+  },
+  {
+    "revision": "e20521c326b88172a5fa",
+    "url": "/static/js/2.47c33752.chunk.js"
+  },
+  {
+    "revision": "7ee8477a6d062bb207d1",
+    "url": "/static/js/3.06048a91.chunk.js"
+  },
+  {
+    "revision": "0b9eae6ed64b2de78d75",
+    "url": "/static/js/main.2ed7de70.chunk.js"
+  },
+  {
+    "revision": "40c0cbfcc8ee7888fa21",
+    "url": "/static/js/runtime~main.daddcf84.js"
+  },
+  {
+    "revision": "cc89873142570dd862c9dc251c913abe",
+    "url": "/static/media/AddUserCampana.cc898731.svg"
+  },
+  {
+    "revision": "e94e2e866cf870b5f999c4a4b2c09ca1",
+    "url": "/static/media/CerrarSesion.e94e2e86.svg"
+  },
+  {
+    "revision": "9a0b88bc45eed1d5dc9cefc0fa6e8adf",
+    "url": "/static/media/DelUserCampana.9a0b88bc.svg"
+  },
+  {
+    "revision": "960b3c160a589ef5e33c5aee678dbd02",
+    "url": "/static/media/EnviarInv.960b3c16.svg"
+  },
+  {
+    "revision": "239037c72501cca07d60f92b42258817",
+    "url": "/static/media/ListaCampana.239037c7.svg"
+  },
+  {
+    "revision": "40b89e1a8a185c6786819353d6612f5b",
+    "url": "/static/media/ListaCategoria.40b89e1a.svg"
+  },
+  {
+    "revision": "ed6921937f2c0881f1a03baf2c22961c",
+    "url": "/static/media/ListaMarcas.ed692193.svg"
+  },
+  {
+    "revision": "3c6d218c1c29b8c650d9e69ba87b8ce0",
+    "url": "/static/media/ListaPlantilla.3c6d218c.svg"
+  },
+  {
+    "revision": "15e87976374033ac1e96f1d19f9da91b",
+    "url": "/static/media/Pazul.15e87976.png"
+  },
+  {
+    "revision": "acc220f6bec7d6844a7103949390cedb",
+    "url": "/static/media/Pliston.acc220f6.png"
+  },
+  {
+    "revision": "efb442a5968acf43b75202ed89d2355c",
+    "url": "/static/media/Pmorado.efb442a5.png"
+  },
+  {
+    "revision": "6ffc5f07a9519d5cc3f220fe84968862",
+    "url": "/static/media/Pregalo1.6ffc5f07.png"
+  },
+  {
+    "revision": "cb8413fdf17b07aae1235485478d80ac",
+    "url": "/static/media/Prosa.cb8413fd.png"
+  },
+  {
+    "revision": "43bb75df4cb27ea8dbead2968b102fa9",
+    "url": "/static/media/administrarperfiles.43bb75df.svg"
+  },
+  {
+    "revision": "e11e1c73ca9e18106cfc41e239fcb268",
+    "url": "/static/media/bg-reedem.e11e1c73.png"
+  },
+  {
+    "revision": "d985ba844700fc67c319850e1122a9ce",
+    "url": "/static/media/bg-register.d985ba84.png"
+  },
+  {
+    "revision": "29c77d78ca2d5244a3ca8d1b0a54383d",
+    "url": "/static/media/inicio.29c77d78.svg"
+  },
+  {
+    "revision": "2a478376497b7ec21fdaf504c8e2c5ef",
+    "url": "/static/media/listaproductos.2a478376.svg"
+  },
+  {
+    "revision": "a9eda354aec908a956fd543e46c6dfb7",
+    "url": "/static/media/loginimg.a9eda354.png"
+  },
+  {
+    "revision": "c4269a8d21a2e2717e36b92873e0d901",
+    "url": "/static/media/regalos.c4269a8d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c7604e75e8bf62883defe1b127ea5e54.js"
+  "/precache-manifest.789b750b763098bdb97f587de988e432.js"
 );
 
 self.addEventListener('message', (event) => {
